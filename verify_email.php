370faefcf476efac542b544117eca294d5393220
shivam kumar <66321059+sk-upadhyay@users.noreply.github.com>
2024-05-20 16:38:54 +0530

    Add files via upload

---
+++ verify_email.php
@@ -0,0 +1,26 @@
+<?php
+$curl = curl_init();
+
+$data = [
+    'email' =>$_POST['email'],
+];
+
+$post_data = http_build_query($data);
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://disify.com/api/email",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => $post_data,
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
+}
